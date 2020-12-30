4e0fddb0a18347676368323d27a5fa82bdefa692
Lucius VanSlyke <lu.vanslyke@gmail.com>
2020-12-30 04:25:46 -0500

    commit tree 2d60366d16962dc295c82ba7d3484094698590de

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,30 @@
-# Add your code here
+require 'pry'
+
+class Dog 
+    @@all = []
+    attr_accessor :name 
+
+    def initialize(name)
+        @name = name
+        save
+    end
+
+    def self.all # this class method allows us to call "all_dogs"
+        @@all
+    end
+
+    def self.clear_all 
+        @@all.clear
+    end
+
+    def self.print_all
+        @@all.map do |dog| #all refers to the array that holds each instance of the dog class. dog refers to the specific instance, therefore we are telling that instance to puts its name. 
+            puts dog.name
+        end
+    end
+
+    def save 
+        @@all << self
+    end
+
+end
